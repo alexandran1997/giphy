d9aeaae9505d896e87d03e5eae576890ad4405a3
Glitch (alex-unit6-giphy) <none>
2022-04-13 22:21:05 +0000

    ⏲️🐡 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,16 +1,34 @@
-let giphyAPIurl = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
+let giphyAPIurl="https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=tTVMCPwEb1NapUWHla1pBNt4jKlfEqo1";
+console.log(giphyAPIurl);
+
+let userInput=$("input").val(); 
+//let newURL= `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=pg&api_key=tTVMCPwEb1NapUWHla1pBNt4jKlfEqo1`;
+let randNum = Math.floor(Math.random() * 49);
+
 
 $(".search-button").click(function() {
   fetch(giphyAPIurl)
+  fetch(randNum)
   
     .then(function(response) {
+            console.log(response);
             return response.json();
+    
   })
     
     .then(function(body) {
-        console.log(body.data);
+        console.log(body.data.length);
   })
   
 });
-                         
-                         
+             
+
+
+
+//$(".search-button").click(function() {     
+//fetch(newURL)
+  
+//  .then(function(response) {
+  //          return response.json();
+  //.then(function(data)
+  
